da2be7c5ad95177e228e5d7b4bf86c2bdd132000
Dave Pearson <davep@davep.org>
2017-06-07 14:07:32 +0100

    Actually write a commentary
    ...replacing the previous one that was a partial cut/paste from another
    buffer and which made little sense.

--- qrencode.el
+++ qrencode.el
@@ -13,8 +13,13 @@
 
 ;;; Commentary:
 ;;
-;; qrencode.el provides a simple Emacs interface for looking things up on
-;; qrenco.de.
+;; qrencode.el provides a simple Emacs interface for turning text into
+;; text-based QR codes, via qrenco.de. Originally written as a bit of a
+;; lunchtime hack after seeing:
+;;
+;; https://twitter.com/asciinema/status/872410405976584192
+;;
+;; on twitter.
 
 ;;; Code:
 
